1a0162186f132c11da8b733789dabda05e8b4516
KostinAV <kostinav4@sovcombank.ru>
2019-09-10 17:42:58 +0400

    Added simple map

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -4,9 +4,25 @@
                 text-center
                 wrap
         >
-            <v-flex xs12>
-              <h2>{{message}}</h2>
+            <v-flex offset-xs1 xs10>
+                <div id="map" style="width: auto; height: 500px"></div>
             </v-flex>
+            <v-flex xs6>
+                <v-text-field
+                        :value="leftBottom"
+                        readonly
+                        label="Left bottom corner:">
+                </v-text-field>
+            </v-flex>
+            <v-flex xs6>
+                <v-text-field
+                        :value="rightTop"
+                        readonly
+                        label="Right top corner:">
+                </v-text-field>
+            </v-flex>
+            <v-btn @click="addObjects">Add objects</v-btn>
+            <v-btn @click="clear">Clear</v-btn>
         </v-layout>
     </v-container>
 </template>
@@ -14,11 +30,46 @@
 <script>
     export default {
         name: "map-component",
+        created() {
+            ymaps.ready(this.initMap);
+        },
         data() {
             return {
-                message:"There will be map"
+                leftBottom: null,
+                rightTop: null,
+                map: {},
+                message: "There will be map",
             }
         },
+        methods: {
+            initMap() {
+                let map = new ymaps.Map("map", {
+                    center: [55.76, 37.64],
+                    zoom: 7,
+                });
+
+                map.events.add('boundschange', this.onSizeChange);
+
+                this.leftBottom = map.getBounds()[0];
+                this.rightTop = map.getBounds()[1];
+                this.map = map;
+            },
+            addObjects() {
+                this.map.geoObjects.add(new ymaps.Placemark([55.8, 37.8], {
+                    balloonContent: 'Test'
+                }, {
+                    preset: 'islands#icon',
+                    iconColor: '#0095b6'
+                }))
+            },
+            clear() {
+                this.map.geoObjects.removeAll();
+            },
+            onSizeChange(e) {
+                this.leftBottom = e.get('oldBounds')[0];
+                this.rightTop = e.get('oldBounds')[1];
+            }
+        }
     }
 </script>
 
